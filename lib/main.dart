5c78280e72321f9b25c7fcc8e39f84327700ce49
aishik <kirtuaishik@gmail.com>
2019-03-04 21:37:30 +0530

    random word generator

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context){
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: "hello",
       home: Scaffold(
         appBar: AppBar(
-          title: Text("This Is My First Flutter App"),
+          title: Text("Random Word Generator"),
         ),
-        body:Center(
-          child: Text("Hello how Are You"),
+        body: Center(
+          //child: Text("Hello how Are You"),
+          child: Text(wordPair.asPascalCase),
           ),
       ),
     );
